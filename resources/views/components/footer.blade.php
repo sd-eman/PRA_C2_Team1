6a8b3410ff699176fffe73a45b5496a234d1f0a2
DirkKuijpers <Dirk.p.Kuijpers@gmail.com>
2026-03-02 12:00:04 +0100

    Footer uitbreiding
    Over ons kopje toegevoegd, Contact gegevens met dummie gegevens, Social media gegevens

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,28 +1,55 @@
-
 <footer class="footer-main">
-	© {{ __('misc.copyright') }}
-</footer>
 
+    <div class="footer-container">
+
+        <!-- Over ons -->
+        <div class="footer-section">
+            <h4>Over ons</h4>
+            <p>Wij zijn een fictief bedrijf dat geweldige websites maakt.</p>
+        </div>
+
+        <!-- Contact -->
+        <div class="footer-section">
+            <h4>Contact</h4>
+            <p>Email: info@voorbeeld.nl</p>
+            <p>Telefoon: 012-3456789</p>
+            <p>Adres: Straatnaam 123, 1234 AB Plaats</p>
+        </div>
+
+        <!-- Social Links -->
+        <div class="footer-section">
+            <h4>Volg ons</h4>
+            <ul class="social-links">
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Instagram</a></li>
+                <li><a href="#">LinkedIn</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+    <!-- Copyright -->
+    <div class="footer-bottom">
+       © {{ __('misc.copyright') }}
+    </div>
+
+</footer>
 
 <!-- analytics code -->
 <script type="text/javascript">
-
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-30506707-1']);
   _gaq.push(['_trackPageview']);
-
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 </script>
-<!-- Einde analytics code -->
 
 <script language="Javascript" type="text/javascript">
-
  if (top.location!= self.location) {
   top.location = self.location.href
  }
-
 </script>
